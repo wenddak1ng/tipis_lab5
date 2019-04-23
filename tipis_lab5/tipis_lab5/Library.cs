59bf43265da9fafb9819b142a23e5042873f1a1c
Vitaly <49898464+wenddak1ng@users.noreply.github.com>
2019-04-23 18:57:57 +0700

    Add files via upload

---
+++ tipis_lab5/tipis_lab5/Library.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace MassConverter {
+
+  public static class Converter {
+
+    static double result;
+    const double pound = 0.4536;
+    const double gramm = 0.001;
+    const double ounce = 31.1035 * gramm;
+    const double slug = 14.5939;
+
+    public static double ConvertFromKgToPound(double mass) {
+      result = mass / pound;
+      return result;
+    }
+
+    public static double ConvertFromKgToOunce(double mass) {
+      result = mass * ounce;
+      return result;
+    }
+
+    public static double ConvertFromOunceToGr(double mass) {
+      result = mass * gramm;
+      return result;
+    }
+
+    public static double ConvertFromKgToSlug(double mass) {
+      result = mass / slug;
+      return result;
+    }
+  }
+
+  public class Output {
+    public double ounce;
+    public double gramm;
+    public static Output Add_Multiply(double a, double b) {
+      var res = new Output {
+          ounce = a,
+          gramm = b
+      };
+      return res;
+    }
+  }
+}
